5de15afb4abc40bae759be7fe872612bc14c1b60
ShambhavII-Singh <shambhavi.singh.work@gmail.com>
2025-05-20 12:34:56 +0530

    editting descriptions

--- Sorting Algorithms/quick_sort_recursive.py
+++ Sorting Algorithms/quick_sort_recursive.py
@@ -1,9 +1,3 @@
-# objective is to partition the array around a pivot element such that all elements smaller are to its left and the larger elements are to its right
-# divide and conquer algorithm using recursion
-# find the correct position of the pivot by using partition and then repeat the process on arrays to the left and right of the pivot
-# base case is when subarrays become a single element as that is already sorted
-# partition process is inplace but due to recursion calls space required is not O(1)
-
 '''
 O(n.logn) : TIME COMPLEXITY (worst case: O(n^2))
 O(n) : SPACE COMPLEXITY (best case: O(logn))
--- Sorting Algorithms/selection_sort.py
+++ Sorting Algorithms/selection_sort.py
@@ -1,10 +1,3 @@
-# objective is to select the minimum element and place them sequentially at the correct index
-# with each iteration the array to be checked for minimum is trimmed
-# have to find the smallest element n times to sort where n is the number of elements (loop 1)
-# if the current minimum element is larger than the current element, minimum element is changed (loop 2)
-# minimum element is placed at the i-th position at the end (loop 2)
-# the array checked decreses in size as with each iteration i-th minimum element is found (loop 2)
-
 '''
 O(n^2) : TIME COMPLEXITY
 O(1) : SPACE COMPLEXITY
